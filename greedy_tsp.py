6096c561e3e8abee7fb661989274fdfbf6caaf61
Ramez <rameznoshy@gmail.com>
2020-05-31 04:22:57 +0200

    feat: interactive greedy plotting

--- greedy_tsp.py
+++ greedy_tsp.py
@@ -1,20 +1,5 @@
-import random
 import matplotlib.pyplot as plt
-import math
-import numpy as np
-import itertools
-
-
-class City:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def distance(self, city):
-        return np.hypot(self.x - city.x, self.y - city.y)
-
-    def __repr__(self):
-        return f"({self.x}, {self.y})"
+from util import City, read_cities, write_cities_and_return_them, generate_cities
 
 
 def path_cost(route):
@@ -29,46 +14,48 @@ class Greedy:
         self.unvisited = cities[1:]
         self.route = [cities[0]]
 
-    def run(self):
+    def run(self, plot):
+        if plot:
+            plt.ion()
+            plt.show(block=False)
+            self.init_plot()
         while len(self.unvisited):
             index, nearest_city = min(enumerate(self.unvisited),
                                       key=lambda item: item[1].distance(self.route[-1]))
             self.route.append(nearest_city)
             del self.unvisited[index]
+            self.plot_interactive(False)
+        self.route.append(self.route[0])
+        self.plot_interactive(False)
+        self.route.pop()
         return path_cost(self.route)
+
+    def plot_interactive(self, block):
+        x1, y1, x2, y2 = self.route[-2].x, self.route[-2].y, self.route[-1].x, self.route[-1].y
+        plt.plot([x1, x2], [y1, y2], 'ro')
+        plt.plot([x1, x2], [y1, y2], 'g')
+        plt.draw()
+        plt.pause(0.3)
+        plt.show(block=block)
+
+    def init_plot(self):
+        fig = plt.figure(0)
+        fig.suptitle('Greedy TSP')
+        x_list, y_list = [], []
+        for city in [*self.route, *self.unvisited]:
+            x_list.append(city.x)
+            y_list.append(city.y)
+        x_list.append(self.route[0].x)
+        y_list.append(self.route[0].y)
+
+        plt.plot(x_list, y_list, 'ro')
+        plt.show(block=False)
 
 
 if __name__ == "__main__":
-    cities = []
-    with open('cities_1024.data', 'r') as handle:
-        lines = handle.readlines()
-        for line in lines:
-            x, y = map(int, line.split())
-            cities.append(City(x, y))
+    cities = read_cities(64)
     greedy = Greedy(cities)
-    # greedy = Greedy([City(x=int(random.random() * 200), y=int(random.random() * 200)) for _ in range(32)])
-    print(greedy.run())
+
+    print(greedy.run(plot=True))
     print(greedy.route)
-    x = []
-    y = []
-    fig = plt.figure(0)
-    fig.suptitle('Greedy TSP')
-    x_list, y_list = [], []
-    for city in greedy.route:
-        x_list.append(city.x)
-        y_list.append(city.y)
-    x_list.append(greedy.route[0].x)
-    y_list.append(greedy.route[0].y)
-
-    plt.plot(x_list, y_list, 'ro')
-    plt.plot(x_list, y_list)
     plt.show(block=True)
-
-    plt.plot(x, y, 'ro')
-    plt.show()
-    # divideConquer.shows_particles()
-    # print(f'gbest: {pso.gbest.pbest}\t| cost: {pso.gbest.pbest_cost}')
-    # plt.plot(pso.gcost_iter)
-    # plt.ylabel('Distance')
-    # plt.xlabel('Generation')
-    # plt.show()

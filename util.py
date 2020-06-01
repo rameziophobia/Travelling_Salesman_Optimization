bd1bf0a00863ed870d658a0a4df69ed9864fb1d7
Ramez <rameznoshy@gmail.com>
2020-06-01 19:48:42 +0200

    hotfix: util, test: add attk48,  eil51 and ulysses16 dataset
    source: http://elib.zib.de/pub/mp-testdata/tsp/tsplib/tsp/
    hotfix: add /test_data to path in util

--- util.py
+++ util.py
@@ -17,17 +17,17 @@ class City:
 
 def read_cities(size):
     cities = []
-    with open(f'cities_{size}.data', 'r') as handle:
+    with open(f'test_data/cities_{size}.data', 'r') as handle:
         lines = handle.readlines()
         for line in lines:
-            x, y = map(int, line.split())
+            x, y = map(float, line.split())
             cities.append(City(x, y))
     return cities
 
 
 def write_cities_and_return_them(size):
     cities = generate_cities(size)
-    with open(f'cities_{size}.data', 'w+') as handle:
+    with open(f'test_data/cities_{size}.data', 'w+') as handle:
         for city in cities:
             handle.write(f'{city.x} {city.y}\n')
     return cities
